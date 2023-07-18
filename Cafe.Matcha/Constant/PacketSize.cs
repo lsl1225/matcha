482f6319b3460520348668adb0f8b2e9c33cded0
lsl1225 <24491036+lsl1225@users.noreply.github.com>
2023-07-18 17:22:52 +0800

    refactor: network monitor code rebase

---
+++ Cafe.Matcha/Constant/PacketSize.cs
@@ -0,0 +1,23 @@
+﻿namespace Cafe.Matcha.Constant
+{
+    public class PacketSize
+    {
+
+        public const int DirectorStart = 168;
+        public const int NpcSpawn = 672;
+        public const int ActorControl = 56;
+        public const int FateInfo = 56;
+        public const int ActorControlSelf = 64;
+        public const int ContentFinderNotifyPop = 72;
+        public const int CompanyAirshipStatus = 176;
+        public const int CompanySubmersibleStatus = 176;
+        public const int InitZone = 136;
+        public const int EventPlay = 72;
+        public const int MarketBoardItemListingCount = 48;
+        public const int MarketBoardItemListing = 1560;
+        public const int ItemInfo = 96;
+        public const int InventoryTransaction = 80;
+        public const int Examine = 1016;
+
+    }
+}

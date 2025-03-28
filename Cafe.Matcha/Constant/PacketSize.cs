68e1f9619472bbeedfcfe33795f05d4cc55369fa
lsl1225 <24491036+lsl1225@users.noreply.github.com>
2025-03-28 16:37:36 +0800

    fix: correct NpcSpawn packet size

--- Cafe.Matcha/Constant/PacketSize.cs
+++ Cafe.Matcha/Constant/PacketSize.cs
@@ -6,14 +6,14 @@ namespace Cafe.Matcha.Constant
     public class PacketSize
     {
         public const int ResumeEventScene32 = 168;
-        public const int NpcSpawn = 680;
+        public const int NpcSpawn = 688; // 7.2 Fix (original: 680)
         public const int ActorControl = 56;
         public const int FateInfo = 56;
         public const int ActorControlSelf = 64;
         public const int ContentFinderNotifyPop = 72;
         public const int CompanyAirshipStatus = 176;
         public const int CompanySubmersibleStatus = 176;
-        public const int InitZone = 144; // 7.1 Fix (original: 136)
+        public const int InitZone = 144;
         public const int EventPlay = 72;
         public const int MarketBoardItemListingCount = 40;
         public const int MarketBoardItemListing = 1480;
